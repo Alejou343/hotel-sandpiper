d9e951c858ffcf3f80beefc385ccdb089a3bddf6
Alejou343 <alejandro.auribe1@gmail.com>
2024-01-25 10:45:41 -0500

    Fix: Warnings in assets

--- src/components/SideHeader/index.jsx
+++ src/components/SideHeader/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
+// import './index.css'
 
 const Index = ({ to }) => {
   return (
     <div className='mx-auto bg-white w-2/3 rounded-[4rem] rounded-ee-none py-2 mb-8'>
         <Link href={to}>
-            <Image src="/Pocki.svg" alt="pocki.svg" width={120} height={120} className='mx-auto' />
+            <Image src="/Pocki.svg" alt="pocki.svg" width={120} height={120} className='mx-auto w-[8rem]' priority={true} />
         </Link>
         <p className='text-center text-xs text-[#7fbd42] font-bold'> Inmobiliario </p>
     </div>
