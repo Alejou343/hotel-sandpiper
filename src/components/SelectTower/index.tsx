8b68224bbcb5b03e43d01bfbd996c9e979be39a3
Alejou343 <alejandro.auribe1@gmail.com>
2024-07-31 10:52:46 -0500

    Feat: Dinamic towers button

--- src/components/SelectTower/index.tsx
+++ src/components/SelectTower/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import RoomStateForm from '@/containers/RoomStateForm'
-import HKAssignForm from '@/containers/HKAssignForm'
-import ShowAssignTable from '@/containers/ShowAssignTable'
-import HKAssignments from '@/containers/HKAssignments'
-import RoomItems from '@/components/RoomItems'
 import Link from 'next/link'
+import axios from 'axios';
 
 interface buttonsTypes {
     id: number;
@@ -12,26 +8,36 @@ interface buttonsTypes {
     endpoint: string
 }
 
+interface buttons {
+    hotelName: string 
+}
 
 const Index: React.FC = () => {
 
-    const [itemSelected, setItemSelected] = React.useState<number>(0)
+    const [buttons, setButtons] = React.useState<string[]>(['', ''])
     const notSelectedStyle: string = 'bg-primary rounded-md px-4 min-w-[15rem] text-center p-4 text-white font-semibold cursor-pointer'
 
-    const buttons: buttonsTypes[] = [
-        { id: 0, name: 'Sandpiper', endpoint: 'Sandpiper' },
-        { id: 1, name: 'Heron', endpoint: 'Heron' },
-    ]
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${process.env.BACK_LINK}/api/towers/`);
+                const allKeys = response?.data?.data?.map((tower: buttons ) => tower.hotelName);
+                setButtons(Array.from(new Set(allKeys)));
+            } catch (err: any) {
+                console.error(err);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div>
             <div className="min-w-[70rem] rounded-md grid grid-cols-2 gap-2">
-                {buttons.map(button =>                       
-                    <Link href={`/room/${button.endpoint}`} key={button.id}>
-                        <div
-                            className={ notSelectedStyle }
-                            onClick={() => setItemSelected(button?.id)}>
-                                {button?.name}
+                {buttons.map((button, id) =>                       
+                    <Link href={`/room/${button}`} key={id}>
+                        <div className={ notSelectedStyle }>
+                            {button}
                         </div>
                     </Link>
                 )}
